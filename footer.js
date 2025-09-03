20b68a072bbf874b58a4016c0c25080ae620b309
norahrid <norah.ridley@usask.ca>
2025-09-03 14:55:07 -0600

    Fix missing href in services.html

--- footer.js
+++ footer.js
@@ -7,9 +7,6 @@ class navFooter extends HTMLElement {
     this.innerHTML = `
       <footer class="col-layout">
         <p>© Terri Karpish Coaching 2025</p>
-        <a href="https://ca.linkedin.com/in/terri-karpish-58902072" class="social-icon si-rounded si-small si-linkedin">
-          <i class="fa-brands fa-linkedin fa-stack-2x fa-inverse"></i>
-        </a>
       </footer>`;
   }
 }
